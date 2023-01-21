ad04daf9f544696fa9fb8cf7b359f9debb39b7d3
HenriGrant <mhggoulet@gmail.com>
2023-01-21 13:13:26 -0500

    Initial Commit

---
+++ MNAP/MNAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNAPApp.swift
+//  MNAP
+//
+//  Created by Michael Goulet on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MNAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
